b220df072ed1a83610144b1d769ccbfc8ce8fa59
Jamie Mansfield <dev@jamierocks.uk>
2016-09-27 20:05:41 +0100

    Improve the colours

--- controllers/github.go
+++ controllers/github.go
@@ -21,14 +21,14 @@ func GetGithub(ctx *macaron.Context) {
         compare := utils.GetGitioUrl(*res.Compare)
 
         modules.BOT.Privmsg("#" + modules.CONFIG.Section("IRC").Key("channel").String(),
-            "[\x0303" + *res.Repo.Name + "\x0F] " + *res.Pusher.Name + " pushed \x02" + strconv.Itoa(len(res.Commits)) + "\x0F commits to " + branch + " " + compare)
+            "\x0303[" + *res.Repo.Name + "]\x0F " + *res.Pusher.Name + " pushed \x02" + strconv.Itoa(len(res.Commits)) + "\x0F commits to " + branch + " " + compare)
 
         for _, commit := range res.Commits {
             message := utils.GetShortCommitMessage(*commit.Message)
             id := utils.GetShortCommitID(*commit.ID)
 
             modules.BOT.Privmsg("#" + modules.CONFIG.Section("IRC").Key("channel").String(),
-                "\x0303" + *res.Repo.Name + "/" + branch + "\x0F " + id + ": \x0315" + message + "\x0F (\x0313By " + *commit.Author.Name + ")\x0F")
+                "\x0303" + *res.Repo.Name + "\x0F/\x0308" + branch + "\x0F " + id + ": \x0315" + message + "\x0F \x0313(By " + *commit.Author.Name + ")\x0F")
         }
     }
 }
