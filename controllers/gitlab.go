c3b3630405ff91ac5335e8c8a3a7a3dd847dc53c
Jamie Mansfield <dev@jamierocks.uk>
2016-09-27 20:12:39 +0100

    Fix up the colours

--- controllers/gitlab.go
+++ controllers/gitlab.go
@@ -19,14 +19,14 @@ func GetGitlab(ctx *macaron.Context) {
         branch := utils.GetBranchName(*res.Ref)
 
         modules.BOT.Privmsg("#" + modules.CONFIG.Section("IRC").Key("channel").String(),
-            "\x0303[" + *res.Repository.Name  + "]\x0F " + *res.UserName + " pushed \x02" + strconv.Itoa(len(res.Commits)) + "\x0F commits to " + branch)
+            "\x0313[" + *res.Repository.Name + "]\x0F " + *res.UserName + " pushed \x02" + strconv.Itoa(len(res.Commits)) + "\x0F commits to " + branch)
 
         for _, commit := range res.Commits {
             message := utils.GetShortCommitMessage(*commit.Message)
             id := utils.GetShortCommitID(*commit.ID)
 
             modules.BOT.Privmsg("#" + modules.CONFIG.Section("IRC").Key("channel").String(),
-                "\x0303" + *res.Repository.Name + "\x0F/\x0308" + branch + "\x0F " + id + ": \x0315" + message + "\x0F \x0313(By " + *commit.Author.Name + ")\x0F")
+                "\x0303" + *res.Repository.Name + "\x0F/\x0307" + branch + "\x0F " + id + ": \x0315" + message + "\x0F \x0313(By " + *commit.Author.Name + ")\x0F")
         }
     }
 }
